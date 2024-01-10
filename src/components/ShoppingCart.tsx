2c93cd2c326345b836b9dcb9a39fa1e5c8b397a7
kingkaminseo <136690999+kingkaminseo@users.noreply.github.com>
2024-01-10 23:10:59 +0900

    쇼핑몰 장바구니 기능구현

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,23 +1,33 @@
-import { Offcanvas, Stack } from "react-bootstrap";
-import { useShoppingCart } from "../context/ShoppingCartContext";
+import { Offcanvas, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { CartItem } from "./CartItem"
+import { formatCurrency } from "../utilities/formatCurrency"
+import  StoreItems  from "../data/items.json"
 
 type ShoppingCartProps = {
-    isOpen: boolean
+  isOpen: boolean
 }
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-    const { closeCart, cartItems } = useShoppingCart()
-    return ( <Offcanvas show= {isOpen} onHide = {closeCart} placement="end">
-        <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Cart</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                <Stack gap={3}>
-                    {cartItems.map(item => (    
-                    <CartItems key={item.id} {...item} />
-                    ))}
-                </Stack>
-            </Offcanvas.Body>
+  const { closeCart, cartItems } = useShoppingCart()
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map(item => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total {formatCurrency(cartItems.reduce((total, CartItem) => {
+                    const item = StoreItems.find(i => i.id === CartItem.id)
+                    return total + (item?.price || 0)* CartItem.quantity 
+            }, 0))}
+          </div>
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
-    )
+  )
 }
